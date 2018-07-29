b68c31e6c72e05b7fda525f971317be4e1b4e2f9
Naqeeb Malik <naqeeb0004@gmail.com>
2018-07-29 23:02:29 +0100

    a

--- public/index.js
+++ public/index.js
@@ -24,7 +24,7 @@ let portrait = false;
 if (x < y) {
   portrait = true;
 }
-if (portrait) document.body.requestFullscreen();
+if (portrait) document.documentElement.requestFullscreen();
 
 let touchx = 0;
 let mobileButtons = [
